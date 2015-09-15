6d4f80fab1749e59d49885db83e15ca0cc64fb34
Amy Wong <columbawong@gmail.com>
2015-09-15 17:07:15 +1000

    MergeThreeEdgesSpec: add tests that when all matched box edges are the same length as the polygon correspondant

--- src/test/scala/org/jinilover/geometry/coordinate/MergeThreeEdgesSpec.scala
+++ src/test/scala/org/jinilover/geometry/coordinate/MergeThreeEdgesSpec.scala
@@ -4,7 +4,7 @@ import org.jinilover.geometry.coordinate.Geometry._
 import org.scalatest.{Matchers, FlatSpec}
 
 class MergeThreeEdgesSpec extends FlatSpec with Matchers {
-  it should "have merge a poly with a bottom box to form 'T'" in {
+  it should "merge with a bottom box to form 'T'" in {
     merge {
       Polygon(
         (3, 10), (6, 10), (6, 8), (10, 8),
@@ -22,7 +22,7 @@ class MergeThreeEdgesSpec extends FlatSpec with Matchers {
     }
   }
 
-  it should "have merge a poly with a bottom box to form 'T' clockwise 90deg" in {
+  it should "merge with a left box to form 'T' clockwise 90deg" in {
     merge {
       Polygon(
         (9, 20), (13, 20), (13, 8), (9, 8),
@@ -40,7 +40,7 @@ class MergeThreeEdgesSpec extends FlatSpec with Matchers {
     }
   }
 
-  it should "have merge a poly with a bottom box to form 'T' inverted" in {
+  it should "merge with a top box to form 'T' inverted" in {
     merge {
       Polygon(
         (3, 10), (12, 10), (12, 5), (10, 5),
@@ -58,7 +58,7 @@ class MergeThreeEdgesSpec extends FlatSpec with Matchers {
     }
   }
 
-  it should "have merge a poly with a bottom box to form 'T' counter clockwise 90deg" in {
+  it should "merge with a right box to form 'T' counter clockwise 90deg" in {
     merge {
       Polygon(
         (3, 20), (11, 20), (11, 17), (8, 17),
@@ -72,6 +72,66 @@ class MergeThreeEdgesSpec extends FlatSpec with Matchers {
           (3, 20), (11, 20), (11, 17), (13, 17),
           (13, 14), (11, 14), (11, 11), (3, 11)
         )
+      )
+    }
+  }
+
+  it should "merge with a bottom box to form a rectangle, i.e. all matched edges same lengths as polygon's correspondants" in {
+    merge {
+      Polygon(
+        (3, 10), (6, 10), (6, 8), (10, 8),
+        (10, 10), (14, 10), (14, 5), (3, 5)
+      )
+    } {
+      Box((6, 8), (10, 10))
+    } should be {
+      Some(
+        Polygon((3, 10), (14, 10), (14, 5), (3, 5))
+      )
+    }
+  }
+
+  it should "merge with a left box to form a rectangle, i.e. all matched edges same lengths as polygon's correspondants" in {
+    merge {
+      Polygon(
+        (9, 20), (13, 20), (13, 8), (9, 8),
+        (9, 12), (11, 12), (11, 16), (9, 16)
+      )
+    } {
+      Box((9, 12), (11, 16))
+    } should be {
+      Some(
+        Polygon((9, 20), (13, 20), (13, 8), (9, 8))
+      )
+    }
+  }
+
+  it should "merge with a top box to form a rectangle, i.e. all matched edges same lengths as polygon's correspondants" in {
+    merge {
+      Polygon(
+        (3, 10), (12, 10), (12, 5), (10, 5),
+        (10, 7), (6, 7), (6, 5), (3, 5)
+      )
+    } {
+      Box((6, 5), (10, 7))
+    } should be {
+      Some(
+        Polygon((3, 10), (12, 10), (12, 5), (3, 5))
+      )
+    }
+  }
+
+  it should "merge with a right box to form a rectangle, i.e. all matched edges same lengths as polygon's correspondants" in {
+    merge {
+      Polygon(
+        (3, 20), (11, 20), (11, 17), (8, 17),
+        (8, 14), (11, 14), (11, 11), (3, 11)
+      )
+    } {
+      Box((8, 14), (11, 17))
+    } should be {
+      Some(
+        Polygon((3, 20), (11, 20), (11, 11), (3, 11))
       )
     }
   }
