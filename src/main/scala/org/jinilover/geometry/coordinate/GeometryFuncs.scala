8bef6637aa5e80153898a522de3f43e941be8f5a
Amy Wong <columbawong@gmail.com>
2015-09-16 06:06:33 +1000

    add GeometryFuncs for the unite func

---
+++ src/main/scala/org/jinilover/geometry/coordinate/GeometryFuncs.scala
@@ -0,0 +1,40 @@
+package org.jinilover.geometry.coordinate
+
+import PolygonFuncs._
+
+object GeometryFuncs {
+  def unite(boxes: Box*): List[Polygon] = {
+    import scalaz.std.option.optionLast
+    import scalaz.std.list._
+    import scalaz.std.tuple._
+    import scalaz.syntax.monoid._
+    import scalaz.LastOption
+    import scalaz.Tags._
+
+    lazy val mergeBoxesToPolygon: BOXES => Polygon => POLYGONS =
+      origBoxes => origPoly => {
+        val (newPoly, remainedBoxes) = origBoxes.foldLeft(
+          (Last(Some(origPoly)): LastOption[Polygon], mzero[BOXES])
+        ) {
+          (z, box) =>
+            z |+| Last.unwrap(z._1).flatMap(combine(_)(box)).fold {
+              (z._1, List(box))
+            } { 
+              p => (Last(Some(p)), Nil)
+            }
+        }
+
+        if (origBoxes == remainedBoxes)
+          origPoly :: unite(remainedBoxes: _*)
+        else
+          Last.unwrap(newPoly).map {
+            mergeBoxesToPolygon(remainedBoxes)
+          }.toList.flatten
+      }
+
+    boxes.toList match {
+      case b :: bs => mergeBoxesToPolygon(bs)(boxToPolygon(b))
+      case _ => Nil
+    }
+  }
+}
--- src/main/scala/org/jinilover/geometry/coordinate/package.scala
+++ src/main/scala/org/jinilover/geometry/coordinate/package.scala
@@ -2,7 +2,9 @@ package org.jinilover.geometry
 
 package object coordinate {
   type POINTS = List[Point]
+  type BOXES = List[Box]
   type EDGES = List[Edge]
+  type POLYGONS = List[Polygon]
   type JOIN_EDGES = EDGES => EDGES => EDGES => EDGES => Option[EDGES]
 
   implicit def toPoint(tuple: (Int, Int)): Point =
